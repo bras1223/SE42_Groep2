9f64512751cd0a1da0f30dfd70d570345a8c792e
Raymond <Ranray@users.noreply.github.com>
2017-05-20 16:44:39 +0200

    Added the Painting domain class

---
+++ auction (associaties)/src/main/java/auction/domain/Painting.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auction.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Raymond
+ */
+@Entity
+public class Painting extends Item {
+    
+    private String title;
+    private String painter;
+
+    public Painting() {
+    }
+
+    public Painting(String title, String painter, User seller, Category category, String description) {
+        super(seller, category, description);
+        this.title = title;
+        this.painter = painter;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPainter() {
+        return painter;
+    }
+
+    public void setPainter(String painter) {
+        this.painter = painter;
+    }
+    
+}
